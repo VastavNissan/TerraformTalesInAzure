25bc21e6ef026a1f1b1ef8e03e9255018df39af2
VastavNissan <88283180+VastavNissan@users.noreply.github.com>
2023-12-25 19:49:02 +0530

    Changed config files as per requirements
    Removed VM cred, resource group name, removed statetf backup file.

---
+++ main.tf
@@ -0,0 +1,62 @@
+# Reference the existing resource group
+data "azurerm_resource_group" "existing" {
+  name = var.resource_group_name
+}
+
+# Create a public IP address
+resource "azurerm_public_ip" "example" {
+  name                = "example-publicip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "example" {
+  name                = "example-machine"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  size                = "standard_b1ls"
+  admin_username      = "Type username as per Azure rules"
+  admin_password      = "Type password as per Azure rules"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-Datacenter"
+    version   = "latest"
+  }
+}
